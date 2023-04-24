75b7af94bba4c63d34560c130be352dd700bf86d
kamikore <kamikore@qq.com>
2023-04-24 16:30:05 +0800

    chore: play -> playground

---
+++ playground/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+      <h1>App component !</h1>
+      <div>
+        <kiButton>123</kiButton>
+      </div>
+      <div>
+        <KiButton @click="handleClick"  round disabled>this is button</KiButton>
+      </div>
+      <div>
+        <kiButton @click="handleClick" type="primary">primary</kiButton>
+      </div>
+      <div>
+        <button disabled @click="handleClick" >click me</button>
+      </div>
+      <div>
+        <button autofocus>autofocus button</button>
+      </div>
+  </div>
+</template>
+
+<script setup>
+function handleClick() {
+  console.log("123")
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
