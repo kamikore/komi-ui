86e0512ba10c1262d948353d929f7109b62a5245
kamikore <kamikore@qq.com>
2023-05-05 13:03:41 +0800

    feat:  add menu component

---
+++ packages/components/menu/index.ts
@@ -0,0 +1,5 @@
+import { withInstall } from '@komi-ui/utils'
+import Menu from './src/Menu.vue'
+
+export const KiMenu = withInstall(Menu)
+export default KiMenu
---
+++ packages/components/menu/src/menu.vue
@@ -0,0 +1,18 @@
+<template>
+    <div :class="[ns.b()]">
+        <slot/>
+    </div>
+</template>
+
+<script setup lang="ts">
+import {ref, defineProps} from 'vue'
+import {menuProps} from './menu'
+import {useNamespace} from '@komi-ui/hooks'
+
+defineOptions({
+    name: "KiMenu"
+})
+
+const ns = useNamespace('menu')
+
+</script>
