6b169473b9855985c435f37827c4368d5c3e5f41
kamikore <kamikore@qq.com>
2023-05-16 23:26:33 +0800

    feat: v-clickoutside arg to excludes triggerElm

--- packages/components/popover/src/popover.vue
+++ packages/components/popover/src/popover.vue
@@ -1,102 +1,127 @@
 <template>
-    <div ref="triggerRef">
-        <slot/>
-    </div>
+    <RenderSlot :vnode="defaultSlot"></RenderSlot>
     <!-- 确保定位相对于body，避免过多的组件嵌套，存在严重副作用 -->
     <teleport to='body'>
-        <div 
-            :class="[ns.b()]" 
-            :style="popoverStyle"
-            ref="popoverRef"
-            v-show="isShow"
-            v-clickoutside="handleClickOutside"
-        >
-            <!-- 默认弹出内容 -->
-            <span v-if="!$slots.content">Content</span>
-            <slot name="content"/>
-            <span 
-                v-if="showArrow"
-                :class="ns.e('arrow')"
-                :style="arrowStyle"
-            ></span>
-        </div>
+        <!-- 初次渲染时应用过渡 -->
+       <Transition appear>
+            <div 
+                :class="[ns.b()]" 
+                :style="popoverStyle"
+                ref="popoverRef"
+                v-show="isShow"
+                v-clickoutside:[triggerRef]="trigger === 'click'?handleClickOutside:''"
+            >
+                <!-- 默认弹出内容 -->
+                <span v-if="!$slots.content">Content</span>
+                <slot name="content"/>
+                <span 
+                    v-if="showArrow"
+                    :class="ns.e('arrow')"
+                    :style="arrowStyle"
+                ></span>
+            </div>
+       </Transition>
     </teleport>    
 </template>
 
 <script lang="ts" setup>
-import {ref, defineProps, onMounted, onUnmounted} from 'vue'
+import {
+    ref, 
+    defineProps,
+    onMounted,
+    onUpdated,
+    onUnmounted,
+    useSlots,
+} from 'vue'
 import {popoverProps} from './popover'
 import {useNamespace} from '@komi-ui/hooks'
 import { vClickoutside } from '@komi-ui/directives'
-import {popIsOverflow,togglePlacement, arrowTransform, getPopStyle,debounce} from '@komi-ui/utils'
-
+import {renderSlot, debounce} from '@komi-ui/utils'
+import { 
+    popIsOverflow, 
+    togglePlacement, 
+    arrowTransform, 
+    getPopStyle
+} from "./utils"
 
 defineOptions({
     name: 'KiPopover'
 })
 
 const props = defineProps(popoverProps)
-
 const ns = useNamespace('popover')
 const triggerRef = ref<HTMLElement | null>(null)
 const popoverRef = ref<HTMLElement | null>(null)
+
+// 缩放监听
+let resizeObserver:ResizeObserver
+let updatePopoverDebounce:Function
+
+const slots = useSlots()
+// slots.default()获取到的是默认插槽所有vnode，但拿不到对应的dom实例, 仅获取第一个vnode
+const defaultSlot = slots.default && slots.default()[0]
+
+const RenderSlot = renderSlot({
+    mountedCallFun: slotMounted
+})
 
 const popoverStyle = ref<any>()
 const arrowStyle = ref<any>()
 
 const pop_placement = ref(props.placement)
 
-
-// 显示元素，为了获取宽高
 const isShow = ref(true)
-popoverStyle.value = {
-    visibility: 'hidden'
-}
 
-// 缩放监听
-let resizeObserver:ResizeObserver
+// 获取slot的DOM
+function slotMounted(slotElm: HTMLElement) {
+    triggerRef.value = slotElm
+}
 
 onMounted(() => {
     // 判断triggerElm是否存在
     // 。。。
-
-    const triggerElm = triggerRef.value?.firstElementChild
+    const triggerElm = triggerRef.value
     const documentElm = document.documentElement
 
     // 缓存popover offsetWidth, offsetHeight
     const {offsetWidth, offsetHeight} = popoverRef.value as HTMLElement
-    
-   
-    resizeObserver = new ResizeObserver(debounce(() => {
-        updatePopover(triggerElm,offsetWidth,offsetHeight)
-    }))
+
+     // 获取宽高后，隐藏元素
+     isShow.value = false
+
+     updatePopoverDebounce = 
+        debounce(() => {
+            updatePopover(triggerElm as Element,offsetWidth,offsetHeight)
+        })
+
+    resizeObserver = new ResizeObserver(updatePopoverDebounce as ResizeObserverCallback)
     // 监听视窗缩放，body存在问题
     resizeObserver.observe(documentElm)
 
     // 监听视窗滚动
-    window.addEventListener('scroll', debounce(() => {
-        updatePopover(triggerElm,offsetWidth,offsetHeight)
-    }))
+    window.addEventListener('scroll', updatePopoverDebounce)
    
     if(props.trigger === 'hover') {
         triggerElm.addEventListener('mouseenter', (event) => isShow.value = true)
         triggerElm.addEventListener('mouseleave', (event) => isShow.value = false)
     } else {
+        // 关闭交由clickOutside关闭
         triggerElm.addEventListener('click', (event) => {
             isShow.value = !isShow.value
         })
     }
-
-    // 获取宽高后，隐藏元素
-    isShow.value = false
-
 })
 
+
+onUpdated(() => {
+    console.log("update hooks");
+    
+})
 
 onUnmounted(() => {
     resizeObserver.disconnect()
+    window.removeEventListener('scroll',updatePopoverDebounce)
 })
-
 
 
 function updatePopover(triggerElm:Element, popWidth:number, popHeight:number) {
@@ -119,9 +144,29 @@ function updatePopover(triggerElm:Element, popWidth:number, popHeight:number) {
     popoverStyle.value = getPopStyle(triggerElm,pop_placement.value,props.showArrow)
 }
 
+
 function handleClickOutside() {
-    isShow.value = false
+    console.log("clickOutside");
+    
+    if(isShow.value) {
+        isShow.value = false
+    }
 }
 
 
 </script>
+
+<style lang="scss" scoped>
+
+/* transition config */
+   .v-enter-active,
+   .v-leave-active {
+     transition: opacity 0.5s ease;
+   }
+
+   .v-enter-from,
+   .v-leave-to {
+     opacity: 0;
+   }
+
+</style>
--- packages/directives/click-outside/index.ts
+++ packages/directives/click-outside/index.ts
@@ -1,5 +1,5 @@
 import type { ObjectDirective, DirectiveBinding } from 'vue'
-
+import { isFunction,isElement } from '@komi-ui/utils'
 
 const nodeList = new Map()
 let startClick: MouseEvent
@@ -12,20 +12,42 @@ document.addEventListener('mouseup', (e:MouseEvent) => {
 })
 
 function createHandler(el:HTMLElement, binding:DirectiveBinding) {
+    
+    // 指令参数列表，额外指定包含的节点
+    let excludes: HTMLElement[] = []
+    if (Array.isArray(binding.arg)) {
+      excludes = binding.arg
+    } else if (isElement(binding.arg)) {
+      excludes.push(binding.arg as unknown as HTMLElement)
+    }
+
 
     return function(mousedownEv:MouseEvent, mouseupEv:MouseEvent) {
-        // el是否是给定节点的后代，即该节点本身、其直接子节点、子节点的直接子节点等。
-        if (!el.contains(mousedownEv.target as Node)) {
-            binding.value(mousedownEv)
+        
+        if (
+            // el是否是给定节点的后代，即该节点本身、其直接子节点、子节点的直接子节点等。
+            el.contains(mousedownEv.target as Node) || 
+            excludes.some((elm) => elm?.contains(mousedownEv.target as Node)) ||
+            excludes.includes(mousedownEv.target as HTMLElement)
+        ) {
+            return
         }
+        binding.value(mousedownEv)
     }
 }
 
 
 // 注意clickoutside做的事：只要元素外点击便触发，元素是否是隐藏状态并不关心
 const clickOutside: ObjectDirective = {
-    beforeMount(el, binding) {
-        if(!nodeList.has(el)) {
+    mounted(el, binding) {
+        if(!nodeList.has(el) && binding.value && isFunction(binding.value)) {
+            nodeList.set(el,createHandler(el,binding))
+        }
+    },
+    updated(el, binding) {
+        
+        // 更新handler
+        if( binding.value && isFunction(binding.value)) {
             nodeList.set(el,createHandler(el,binding))
         }
     },
