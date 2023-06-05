17dc5e7a423b8e509916eb0002c109b76ec284cb
kamikore <kamikore@qq.com>
2023-06-06 00:21:54 +0800

    chore: Install more than one component

--- packages/utils/vue/install.ts
+++ packages/utils/vue/install.ts
@@ -2,15 +2,17 @@ import type { App} from 'vue'
 import { SFCWithInstall } from "./typescript"
 
 // 给组件添加install方法
-export const withInstall = <T>(plugin:T) => {
+export const withInstall = <T, E extends Record<string, any>>(
+    main:T, 
+    extra?:E
+  ) => {
     // 合并T泛型到SFCWithInstall
-  (plugin as SFCWithInstall<T>).install = (app: App): void => {
-    // for (const comp of [main, ...Object.values(extra ?? {})]) {
-    //   app.component(comp.name, comp)
-    // }
-    
-    app.component(plugin.name, plugin)
+  (main as SFCWithInstall<T>).install = (app: App): void => {
+    // 注册多个组件
+    for (const comp of [main, ...Object.values(extra ?? {})]) {
+      app.component(comp.name, comp)
+    }
   }
 
-  return plugin as SFCWithInstall<T>
+  return main as SFCWithInstall<T> & E  // 合并类型
 }
