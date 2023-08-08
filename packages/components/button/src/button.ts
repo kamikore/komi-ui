5c2794b580da900deeb283b91f8399d9ea162536
kamikore <kamikore@qq.com>
2023-08-08 11:03:18 +0800

    type: add iconType

--- packages/components/button/src/button.ts
+++ packages/components/button/src/button.ts
@@ -1,6 +1,7 @@
-import type { Component, PropType } from 'vue'
-import {COMPONENTSIZES, definePropType, buildProps} from '@komi-ui/utils'
+import {COMPONENTSIZES, definePropType, buildProps, iconPropType} from '@komi-ui/utils'
+import { Loading } from '@element-plus/icons-vue'
 
+import type { Component, PropType } from 'vue'
 
 export const buttonProps = buildProps({
     tag: {
@@ -21,14 +22,17 @@ export const buttonProps = buildProps({
         values: ['default','primary',''],
         default: 'default'
     },
-    icon: definePropType<string | Component>([
-        String,
-        Object,
-        Function,
-    ]),
+    icon: iconPropType,
     round: Boolean,
     circle: Boolean,            
     loading: Boolean,
+    /**
+    * @description customize loading icon component
+    */
+    loadingIcon: {
+        type: iconPropType,
+        default: () => Loading,
+    },
     color: String,
     dark: Boolean,
     disabled: Boolean,      
